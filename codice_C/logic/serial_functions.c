c5488dfda61d88280bdeec82e840adf78c0beeb5
StefanoTrv <stefanotravasci@gmail.com>
2023-11-29 17:13:54 +0100

    Implemetato recursive_search() Implementato il codice C per recursive_search(), da testare.

--- codice_C/logic/serial_functions.c
+++ codice_C/logic/serial_functions.c
@@ -39,6 +39,85 @@ int* gale_shapley(int n, int** men_preferences, int** women_preferences) {
     return matching;
 }
 
+void recursive_search(char* matching, int n, struct RotationsListElement* free_rotations_list, struct ResultsList* results_list){
+	struct RotationNode* successor;
+	struct RotationList* new_list_el;
+	struct RotationList* temp;
+	struct RotationsListElement* new_free_rotations_list;
+	struct SuccessorsList* successors_list;
+	struct RotationsList* rotation;
+	struct RotationList* first_new_rotation;
+	
+	while(free_rotations_list != NULL){
+		//applica la rotazione
+		rotation = free_rotations_list->value->value;
+		
+		char first_woman = rotation->woman;
+		while(rotation->next != NULL){
+			matching[rotation->man] = rotation->next->woman;
+			rotation = rotation->next;
+		}
+		matching[rotation->man] = first_woman;
+		
+		//aggiungo il matching ai risultati
+		char* new_matching = malloc(sizeof (char) * n);
+		for(int i=0;i<n;i++){
+			new_matching[i]=matching[i];
+		}
+		appendResultsList(results_list,new_matching);
+		
+		//creo la nuova lista delle rotazioni per la chiamata ricorsiva
+		//tolgo la rotazione applicata e aggiungo le nuove rotazioni
+		first_new_rotation = NULL;
+		new_free_rotations_list = free_rotations_list->next;
+		successors_list = free_rotations_list->value->successors;
+		while(successors_list != NULL){
+			successor = successors_list->value;
+			successor->missing_predecessors -= 1;
+			if (successor->missing_predecessors == 0){
+				//aggiungi questa rotazione in cima alla lista
+				new_list_el = malloc(sizeof (struct RotationList));
+				new_list_el->value = successor;
+				new_list_el->next = new_free_rotations_list;
+				new_free_rotations_list = new_list_el;
+				if (first_new_rotation==NULL){ //per liberare successivamente la memoria
+					first_new_rotation = new_list_el;
+				}
+			}
+		}
+		
+		//chiamata ricorsiva
+		recursive_search(matching, n, new_free_rotations_list, results_list);
+		
+		//ripristino le rotazioni
+		//ripristino i missing_predecessors dei successori
+		successors_list = free_rotations_list->value->successors;
+		while(successors_list != NULL){
+			successor = successors_list->value;
+			successor->missing_predecessors += 1;
+		}
+		
+		//libero la memoria allocata per le nuove rotazioni
+		if (first_new_rotation!=NULL){
+			while(new_list_el!=first_new_rotation) {
+				temp = new_list_el->next;
+				free(new_list_el);
+				new_list_el=temp;
+			}
+			free(new_list_el);
+		}
+		
+		//ripristina rotazione
+		rotation = free_rotations_list->value->value;
+		while(rotation != NULL){
+			matching[rotation->man] = rotation->woman;
+			rotation = rotation->next;
+		}
+		
+		//ad ogni iterazione, togliamo una rotazione dalla lista
+		free_rotations_list = free_rotations_list->next;
+	}
+}
 
 int test(int** women_preferences, int n, int w, int m, int m1) {
     for (int i = 0; i < n; i++) {
